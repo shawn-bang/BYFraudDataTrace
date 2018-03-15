cb4f2fa6d57783dd18145a143f54fc6aa12be467
xiaobingbing <xiaobang6312@sina.com>
2018-03-15 16:48:49 +0800

    shawn:recode

--- src/main/java/com/byit/fraud/trace/FraudDataTraceMain.java
+++ src/main/java/com/byit/fraud/trace/FraudDataTraceMain.java
@@ -15,6 +15,9 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -32,6 +35,7 @@ public class FraudDataTraceMain {
 	private static Map<String, String> AF1001 = null;
 	private static Map<String, String> AF1002 = null;
 	private static int totalCount = 0, successCount = 0, failCount = 0;
+	private static Executor executor = Executors.newFixedThreadPool(2 * Runtime.getRuntime().availableProcessors());
 
 	/**
 	 * 解析单个日志文件
@@ -101,8 +105,16 @@ public class FraudDataTraceMain {
 		}
 
 		// send http message to target web server url: priority first AF1001, second AF1002
+		logger.info("AF1001 run start");
+		long startTime1001 = System.currentTimeMillis();
 		doRequestService(AF1001);
+		long endTime1001 = System.currentTimeMillis();
+		logger.info("AF1001 run end:{}", (endTime1001 - startTime1001) + "");
+		logger.info("AF1002 run start");
+		long startTime1002 = System.currentTimeMillis();
 		doRequestService(AF1002);
+		long endTime1002 = System.currentTimeMillis();
+		logger.info("AF1002 run end:{}", (endTime1002 - startTime1002) + "");
 		logger.info("Total request count:{}", totalCount);
 		logger.info("Success request count:{}", successCount);
 		logger.info("Fail request count:{}", failCount);
@@ -113,16 +125,47 @@ public class FraudDataTraceMain {
 	 * @param map
 	 */
 	private static void doRequestService(Map<String, String> map) {
+		int threadNum;
+		if (map.size() % 5 == 0) {
+			threadNum = map.size() / 5;
+		}else {
+			threadNum = map.size() / 5 + 1;
+		}
+		ThreadListener listener = new ThreadListener(threadNum);//线程监视器
+
+		List<Map<String, String>> l = splitMap(map);
+		for (int i = 0; i < threadNum; i++) {
+			executor.execute(new DoPostServiceThread(l.get(i), listener));
+		}
+
+		//等待所有线程执行完毕，返回对象
+		logger.info("等待所有线程都结束================================");
+		if (listener.isAllThreadsFinished()) {
+
+		} else {
+			throw new RuntimeException("有线程没有正常结束");
+		}
+	}
+
+	private static List<Map<String, String>> splitMap(Map<String, String> map) {
+		if (null == map || map.size() == 0) return null;
+		int count = 0;
+		List<Map<String, String>> l = new ArrayList<>();
+		Map<String, String> divMap = null;
 		for (Map.Entry<String, String> entry : map.entrySet()) {
-			totalCount++;
-			String appId = entry.getKey();
-			String json = entry.getValue();
-			if (httpPostWithJson(json, appId)) {
-				successCount++;
-			}else {
-				failCount++;
+			count++;
+			if (divMap == null) divMap = new HashMap<>();
+			divMap.put(entry.getKey(), entry.getValue());
+			if (count % 5 == 0 && count != map.size()) {
+				l.add(divMap);
+				divMap = new HashMap<>();
+			}
+			if (count == map.size()) {
+				l.add(divMap);
 			}
 		}
+
+		return l;
 	}
 
 	/**
@@ -159,61 +202,131 @@ public class FraudDataTraceMain {
 		return list;
 	}
 
-	private static boolean httpPostWithJson(String json, String appId){
-		boolean flag;
-		HttpPost post = null;
-		try {
-			HttpClient httpClient = new DefaultHttpClient();
+	static class DoPostServiceThread implements Runnable {
 
-			// set timeout
-			httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 1000);
-			httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 1000);
+		private Map<String, String> map;
+		private ThreadListener listener;
 
-			post = new HttpPost(serviceUrl);
-			// construct message headers
-			post.setHeader("Content-type", "application/json; charset=utf-8");
-			post.setHeader("Connection", "close");
-			// construct message entity
-			StringEntity entity = new StringEntity(json, Charset.forName("UTF-8"));
-			entity.setContentEncoding("UTF-8");
-			// set content type
-			entity.setContentType("application/json");
-			post.setEntity(entity);
+		public DoPostServiceThread(Map<String, String> map, ThreadListener listener) {
+			this.map = map;
+			this.listener = listener;
+		}
 
-			HttpResponse response = httpClient.execute(post);
+		@Override
+		public void run() {
+			logger.info("开启一个新的线程：" + Thread.currentThread().getName());
+			long start_time = System.currentTimeMillis();
 
-			// check response result status:1-success, 0-fail
-			int statusCode = response.getStatusLine().getStatusCode();
-			if(statusCode != HttpStatus.SC_OK){
-				logger.warn("Response status exception:{}:{}", appId, statusCode);
-				flag = false;
-			}else {
-				String responseJson = response.getEntity().toString();
-				String status = JSON.parseObject(responseJson).getJSONObject("response").getString("status");
-				if (!status.equals("1")) {
-					logger.warn("Response result status exception:{}:{}", appId, responseJson);
-					flag = false;
-				} else {
-					flag = true;
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+				totalCount++;
+				String appId = entry.getKey();
+				String json = entry.getValue();
+				if (httpPostWithJson(json, appId)) {
+					successCount++;
+				}else {
+					failCount++;
 				}
 			}
-		} catch (Exception e) {
-			logger.error("Request error:{}", appId);
-			logger.error("Request error", e);
-			flag = false;
-		}finally{
-			if(post != null){
-				try {
-					post.releaseConnection();
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					logger.error("Request error:{}", appId);
-					logger.error("Request error", e);
-				}
+
+			logger.debug("====线程结束：" + Thread.currentThread().getName());
+			long end_time = System.currentTimeMillis();
+			logger.debug("耗时：" + (end_time - start_time) / 1000 + " 秒");
+			//监听线程之用
+			try {
+				listener.getQueue().put(Thread.currentThread().getName());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 
-		return flag;
+		private boolean httpPostWithJson(String json, String appId){
+			boolean flag;
+			HttpPost post = null;
+			try {
+				HttpClient httpClient = new DefaultHttpClient();
+
+				// set timeout
+				httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 1000);
+				httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 1000);
+
+				post = new HttpPost(serviceUrl);
+				// construct message headers
+				post.setHeader("Content-type", "application/json; charset=utf-8");
+				post.setHeader("Connection", "close");
+				// construct message entity
+				StringEntity entity = new StringEntity(json, Charset.forName("UTF-8"));
+				entity.setContentEncoding("UTF-8");
+				// set content type
+				entity.setContentType("application/json");
+				post.setEntity(entity);
+
+				HttpResponse response = httpClient.execute(post);
+
+				// check response result status:1-success, 0-fail
+				int statusCode = response.getStatusLine().getStatusCode();
+				if(statusCode != HttpStatus.SC_OK){
+					logger.warn("Response status exception:{}:{}", appId, statusCode);
+					flag = false;
+				}else {
+					String responseJson = response.getEntity().toString();
+					String status = JSON.parseObject(responseJson).getJSONObject("response").getString("status");
+					if (!status.equals("1")) {
+						logger.warn("Response result status exception:{}:{}", appId, responseJson);
+						flag = false;
+					} else {
+						flag = true;
+					}
+				}
+			} catch (Exception e) {
+				logger.error("Request error:{}", appId);
+				logger.error("Request error", e);
+				flag = false;
+			}finally{
+				if(post != null){
+					try {
+						post.releaseConnection();
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						logger.error("Request error:{}", appId);
+						logger.error("Request error", e);
+					}
+				}
+			}
+
+			return flag;
+		}
+	}
+
+	static class ThreadListener {
+		private LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();//利用阻塞队列实现所有线程结束的监听
+		private int threadNum;//线程数目
+
+		ThreadListener(int threadNum) {
+			this.threadNum = threadNum;
+		}
+
+		LinkedBlockingQueue<String> getQueue() {
+			return queue;
+		}
+
+		/**
+		 * 判断是否所有线程都程结束
+		 *
+		 * @return
+		 */
+		boolean isAllThreadsFinished() {
+			for (int i = 0; i < threadNum; i++) {
+				try {
+					String threadName = queue.take();
+					logger.info(threadName + " 线程结束");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+			return true;
+		}
+
 	}
 
 }
